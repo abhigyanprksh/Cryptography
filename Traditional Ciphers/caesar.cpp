3d761d68bc4ffc0d81d37388b4b0587512f0b902
Abhigyan Prakash <31375949+abhigyanprksh@users.noreply.github.com>
2018-09-14 02:49:51 +0530

    Create caesar.cpp

---
+++ Traditional Ciphers/caesar.cpp
@@ -0,0 +1,82 @@
+//Implementation of encryption and decryption in Caesar Cipher
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int ch;
+    cout << "\t\tCAESAR CIPHER\n\nChoose one of the options:\n1. Encryption\n2. Decryption\n";
+    cin >> ch;
+
+    string s;
+    int key;
+
+    switch (ch) {
+    case 1:
+
+        cout << "\nEnter the text: "; //Input should contain single string having alphabets only
+        cin >> s;
+
+        cout << "\nEnter the key value: ";
+        cin >> key;
+
+        cout << "\nThe encrypted text is: ";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (islower(s[i]))
+                cout << char('a' + (s[i] - 'a' + key) % 26);
+            else
+                cout << char('A' + (s[i] - 'A' + key) % 26);
+        }
+
+        cout << "\n";
+
+        break;
+
+    case 2:
+
+        cout << "\nEnter the text: "; //Input should contain single string having alphabets only
+        cin >> s;
+
+        cout << "\nEnter the key value: ";
+        cin >> key;
+
+        cout << "\nThe decrypted text is: ";
+
+        for (int i = 0; i < s.length(); i++) {
+            int shift = key % 26;
+
+            if (islower(s[i])) {
+                if (shift > (s[i] - 'a')) {
+                    shift = shift - s[i] + 'a';
+
+                    cout << char('z' - shift + 1);
+                }
+
+                else
+                    cout << char(s[i] - shift);
+            }
+
+            else {
+                if (shift > (s[i] - 'A')) {
+                    shift = shift - s[i] + 'A';
+
+                    cout << char('Z' - shift + 1);
+                }
+
+                else
+                    cout << char(s[i] - shift);
+            }
+        }
+
+        cout << "\n";
+
+        break;
+
+    default:
+
+        cout << "\nInvalid Input!\n";
+    }
+}
